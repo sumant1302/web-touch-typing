db36f49e647bb855b0be6f6c493eaf5771e154da
Sumant Kumar <sumant130201@gmail.com>
2023-06-10 08:06:08 +0530

    Removing react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter >
-      <Routes>
-        <Route path='/' element={<Input />}></Route>
-      </Routes>
-      </BrowserRouter>
+      <Input />
     </div>
   );
 }
